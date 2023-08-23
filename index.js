8a2327eb67e31db8fc351d66017303335fb75796
Jamar Graves <jamargraves07@gmail.com>
2023-08-23 16:16:18 -0400

    was ez

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {name:"Tom", streetAddress: "Arthur St"}
+ 
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    let  copyofemployee = {...employee}
+copyofemployee[key] = value
+return (copyofemployee);
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+employee[key] = value;
+return(employee)
+}
+function deleteFromEmployeeByKey(employee, key){
+    let copyofemployee = {...employee}
+    delete copyofemployee[key]
+    return copyofemployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee;
+}
